01ca77f577fcbd665cc2072f50c75975e2a831da
jordanhudgens <jordan@devcamp.com>
2019-05-22 15:23:39 -0700

    Added the ability to loop over posts

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
