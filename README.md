fd0f85bb8cf3b1f16be55374b3c576bfdb33db92
jordanhudgens <jordan@devcamp.com>
2019-05-30 12:10:34 -0600

    implemented the ability to encrypt passwords

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 * Start pipenv shell session
 * Ensure that you have the FLASK_DEBUG and FLASK_APP constants in the .flaskenv file
 * python -m flask run
+* To access the shell run: `flask shell`
 
 > Next: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
 
@@ -7,6 +8,12 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
