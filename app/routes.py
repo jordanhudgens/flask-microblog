50f0a4248610f49300359609f013bd4b9dec397d
jordanhudgens <jordan@devcamp.com>
2019-06-20 11:12:09 -0700

    Finished implementing the registration process

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
+from app import app, db
 from werkzeug.urls import url_parse
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
