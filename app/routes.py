10831fde14c4b0e614b8d20dff39b915bc977b20
jordanhudgens <jordan@devcamp.com>
2019-06-20 12:07:30 -0700

    Added a before request callback to the routes

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,14 @@ from app import app, db
 from werkzeug.urls import url_parse
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
+from datetime import datetime
 from app.models import User
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
