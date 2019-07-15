76534f5e347a30ea35c13e71f124251884fc360b
jordanhudgens <jordan@devcamp.com>
2019-07-15 14:43:52 -0700

    Updated the call for the followers table and updated the schema

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from datetime import datetime
 from app import db, login
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -47,8 +52,3 @@ class Post(db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
-
-followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-)
