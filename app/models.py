95add72bc8d9798e42fa3095d3a1faa02b43c415
jordanhudgens <jordan@devcamp.com>
2019-10-22 12:14:34 -0700

    Extended the followed posts query

--- app/models.py
+++ app/models.py
@@ -47,10 +47,11 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def followed_posts(self):
-        return Post.query.join(
+        followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id).order_by(
-                    Post.timestamp.desc())
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
